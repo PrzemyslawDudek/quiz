5b10771084d583e1bb6fe12706ab661c35a8ff47
Przemysław Dudek <dudek.mail@gmail.com>
2021-06-30 16:00:50 +0200

    FEAT: Create Quiz Form

--- src/main/java/pl/dudecode/quiz/QuizForm.java
+++ src/main/java/pl/dudecode/quiz/QuizForm.java
@@ -5,7 +5,12 @@ import lombok.Data;
 @Data
 public class QuizForm {
 
-    public static String NO_ANSWER = "Brak Odpowiedzi";
+    public static final String NO_ANSWER = "Brak Odpowiedzi";
+
+    public static String questionForC = "Co oznacza literka C w akronimie CRUD";
+    public static String questionForR = "Co oznacza literka R w akronimie CRUD";
+    public static String questionForU = "Co oznacza literka U w akronimie CRUD";
+    public static String questionForD = "Co oznacza literka D w akronimie CRUD";
 
     private String answerForC;
     private String answerForR;
